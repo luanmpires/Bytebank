7307829c39ed470f0e6b656d6e37c8a8fe4b1e68
Luan Pires <luanmpires@gmail.com>
2022-05-30 16:14:04 -0300

    first commit

---
+++ Conta/ContaCorrente.js
@@ -0,0 +1,17 @@
+import {Conta} from "./Conta.js";
+
+export class ContaCorrente extends Conta{
+    static numeroDeContas = 0;
+
+    // #saldo =0 https://github.com/tc39/proposal-class-fields#private-fields
+
+    constructor(cliente, agencia) {
+        super(0, cliente, agencia);
+        ContaCorrente.numeroDeContas += 1;
+    }
+
+    sacar(valor){
+        let taxa = 1.1;
+        return this._sacar(valor, taxa);
+    }
+}
